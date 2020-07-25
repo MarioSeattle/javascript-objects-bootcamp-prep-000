9f9d5a202813ed28316a3aa89b2e1195d14096b3
Mario Gomez <mgomez15@seattlecentral.edu>
2020-07-25 18:35:17 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+//Assign an object to the variable playlist and initialize the object with 
+//a key-value pair — the keys will be artist names and the values 
+//will be song titles.
+
+var playlist = {Korn: "Fallen Away"};
+
+//Updated playlistadds the 'artisName: songTitle' key value pair to Object 
+
+playlist.Korn = ["Romeo"];
+
